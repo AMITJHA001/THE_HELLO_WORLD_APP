e8d1f73d8db8c49cf98ee9be1a0f1320f6308718
AMITJHA001 <amit.jhaa17@gmail.com>
2021-02-02 23:01:04 +0530

    Leasted massage

---
+++ javascript/app.js
@@ -0,0 +1,33 @@
+/*var color1 = document.querySelectorAll('.color1');
+var color2 = document.querySelectorAll('.color2');
+var color3 = document.querySelectorAll('.color3');
+var color4 = document.querySelectorAll('.color4');
+var color5 = document.querySelectorAll('.color5');
+var color6 = document.querySelectorAll('.color6');
+var color7 = document.querySelectorAll('.color7');
+var color8 = document.querySelectorAll('.color8');
+var color9 = document.querySelectorAll('.color9');
+var color10 = document.querySelectorAll('.color10');
+var color11 = document.querySelectorAll('.color11');
+
+let randomColor = Math.floor(Math.random() * 16777215).toString(16);*/
+
+var string = document.getElementById("content").innerText;
+var stringwithoutspace = Array.from(string.split(" ").join(""));
+
+stringwithoutspace.forEach(myFunction);
+
+
+
+
+function myFunction(value, index, array) {
+
+
+
+    // let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    // console.log(randomColor);
+
+
+}
+
+//console.log(stringwithoutspace);
